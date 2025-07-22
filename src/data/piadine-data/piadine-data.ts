e5bb7b16b1467f17bc4060bca1f52c877166ab35
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-07-22 10:48:13 +0200

    create types & data for piadina-page component

---
+++ src/data/piadine-data/piadine-data.ts
@@ -0,0 +1,55 @@
+import type { Piadina } from "../../types/piadine-type/piadine-types";
+
+export const piadineClassiche: Piadina[] = [
+  { title: "PROSCIUTTO CRUDO", price: "€ 6,50" },
+  { title: "PROSCIUTTO CRUDO E MOZZARELLA", price: "€ 7,50" },
+  { title: "PROSCIUTTO CRUDO, STRACCHINO, RUCOLA", price: "€ 7,50" },
+  { title: "PROSCIUTTO CRUDO E INSALATA", price: "€ 7,00" },
+  { title: "GRATINATI", price: "€ 7,50" },
+  { title: "ERBE E SALSICCIA", price: "€ 7,50" },
+  { title: "SALAME MILANO", price: "€ 6,00" },
+  { title: "PROSCIUTTO COTTO E MOZZARELLA", price: "€ 6,50" },
+  {
+    title: "PROSCIUTTO CRUDO, MOZZARELLA\nE POMODORI A FETTE",
+    price: "€ 7,50",
+  },
+];
+
+export const piadineSpeciali: Piadina[] = [
+  {
+    title: "PROSCIUTTO CRUDO, PECORINO E INSALATA",
+    price: "€ 8,00",
+  },
+  {
+    title: "SPECK, GORGONZOLA E FUNGHI TRIFOLATI",
+    price: "€ 8,00",
+  },
+  {
+    title: "SALSICCIA, PECORINO E GRATE’",
+    price: "€ 8,50",
+  },
+  {
+    title: "SALSICCIA E CIPOLLA STUFATA",
+    price: "€ 7,50",
+  },
+  {
+    title: "PROSCIUTTO COTTO, MOZZARELLA\nE FUNGHI TRIFOLATI",
+    price: "€ 7,50",
+  },
+  {
+    title: "SALAME PICCANTE, CARCIOFI E MAIONESE",
+    price: "€ 7,00",
+  },
+  {
+    title: "PORCHETTA, INSALATA E PECORINO",
+    price: "€ 8,00",
+  },
+  {
+    title: "BRESAOLA, RUCOLA E GRANA",
+    price: "€ 8,50",
+  },
+  {
+    title: "MORTADELLA, BUFALA\nE GRANELLA DI PISTACCHI",
+    price: "€ 8,00",
+  },
+];
---
+++ src/types/piadine-type/piadine-types.ts
@@ -0,0 +1,4 @@
+export interface Piadina {
+  title: string;
+  price: string;
+}
