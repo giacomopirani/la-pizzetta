4f499231f710b4f01461541556d5f758099b1217
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-07-23 15:06:57 +0200

    refactor fritti-page

--- src/pages/piadine/piadine-header.tsx
+++ src/pages/piadine/piadine-header.tsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import React from "react";
-import piadinaIcon from "../../assets/icon-svg/piadine.svg";
 
 interface PiadineHeaderProps {
   backgroundImage?: string;
@@ -12,7 +11,7 @@ const PiadinaHeader: React.FC<PiadineHeaderProps> = ({ backgroundImage }) => {
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6 }}
-      className="relative py-26 overflow-hidden min-h-[400px] flex items-end justify-center pb-8"
+      className="relative py-26 overflow-hidden min-h-[400px] flex items-end justify-center pb-0"
       style={{
         backgroundImage: backgroundImage
           ? `url(${backgroundImage})`
@@ -27,7 +26,7 @@ const PiadinaHeader: React.FC<PiadineHeaderProps> = ({ backgroundImage }) => {
           initial={{ scale: 0.8, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
           transition={{ delay: 0.2, duration: 0.5 }}
-          className="text-center bg-[rgba(0,0,0,0.5)] backdrop-blur-sm rounded-lg p-8 inline-block"
+          className="text-center bg-[rgba(0,0,0,0.5)] backdrop-blur-sm rounded-t-lg p-2 px-27 inline-block"
         >
           <p
             className="font-bold text-white border-b-2 border-white/80 mb-3 pb-2"
@@ -37,18 +36,10 @@ const PiadinaHeader: React.FC<PiadineHeaderProps> = ({ backgroundImage }) => {
           </p>
           <h1
             style={{ fontFamily: "Hoverage, sans-serif" }}
-            className="text-5xl md:text-6xl font-bold text-white mb-4 tracking-tight drop-shadow-lg"
+            className="text-5xl md:text-6xl font-bold text-white tracking-tight drop-shadow-lg"
           >
             PIADINE
           </h1>
-
-          <img
-            src={piadinaIcon}
-            alt="Icona Cascione"
-            width={80}
-            height={80}
-            className="object-contain mx-auto"
-          />
         </motion.div>
       </div>
     </motion.div>
